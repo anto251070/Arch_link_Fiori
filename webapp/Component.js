7288e6c4ab6d0b5d5de828ea3ee507d66d3f2a2d
AGI <anto251070@gmail.com>
2023-12-24 17:10:00 +0100

    Add files via upload

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ch.unige.lrdocument.Component", {
+            metadata: {
+                manifest: "json",
+            }
+        });
+    }
+);
